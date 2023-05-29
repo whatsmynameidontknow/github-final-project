81d2768148ec6d1a2814b33006b461f301052e73
whatsmynameidontknow <44807964+whatsmynameidontknow@users.noreply.github.com>
2023-05-29 17:39:17 +0700

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -5,8 +5,8 @@
    # Do not use this in production. Sample purpose only.
 
    # Author: Upkar Lidder (IBM)
-   # Additional Authors:
-   # <your GitHub username>
+   # Additional Authors: 
+   # whatsmynameidontknow
 
    # Input:
    # p, principal amount
